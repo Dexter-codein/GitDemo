3c5a5b258f5650f28afd16a409c915891657e243
Dexter-codein <rishabhchauhan59@gmail.com>
2023-10-31 23:58:07 +0530

    this is commit from India guy

--- live2.py
+++ live2.py
@@ -5,7 +5,8 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://www.facebook.com/")
 driver.find_element_by_id("email").send_keys('rishabh')
-driver.find_element_by_id("pass").send_keys('rishabh')
+driver.find_element_by_id("pass").send_keys('Qwerty')
+Print("Hello GIT")
 
 submit_button = driver.find_element_by_xpath("//button[@type='submit']")
 driver.implicitly_wait(10)
